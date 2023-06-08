1ef39e9fb2954421ab0a553270349c7cd776dcdb
MercyCroft <49827267+Mercedesar@users.noreply.github.com>
2023-06-08 10:28:54 -0400

    Update millas.java

--- clase 5/millas.java
+++ clase 5/millas.java
@@ -1,7 +1,7 @@
 //Calculadora de Millas+km 
 import java.util.Scanner;
 
-public class Main {
+public class millas {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Ingrese una distancia en millas: ");
