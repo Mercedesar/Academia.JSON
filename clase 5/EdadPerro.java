7e59dc1e0084d9af6a2486c6467bdb09b1747547
MercyCroft <49827267+Mercedesar@users.noreply.github.com>
2023-06-08 10:24:18 -0400

    Update EdadPerro.java
    Main

--- clase 5/EdadPerro.java
+++ clase 5/EdadPerro.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese la edad de su perro");
+        int edad = scanner.nextInt();
+        System.out.println("La edad de su perro es " + calcularEdadPerros(edad) + " anos");
+    }
+
+    public static int calcularEdadPerros(int edadPerro) {
+        return edadPerro * 7;
+    }
+}
